c797aa150af68c041d006aa38df0c7d51802fc33
DiscoverMyself <78480857+DiscoverMyself@users.noreply.github.com>
2023-02-28 14:23:35 +0700

    Update nibi.sh

--- src/nibi.sh
+++ src/nibi.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+# © 2023 by: Aprame
+
 clear
 echo ""
 echo "Wait ..."
@@ -23,32 +24,37 @@ echo -e "\e[0m"
 
 sleep 1
 
-# set variables
+
+
+
+# Variable
 SOURCE=nibiru
 BINARY=nibid
-FOLDER=.nibid
 CHAIN=nibiru-itn-1
+FOLDER=.nibid
 VERSION=v0.19.2
 DENOM=unibi
 COSMOVISOR=cosmovisor
 REPO=https://github.com/NibiruChain/nibiru.git
 GENESIS=https://snapshot.yeksin.net/nibiru/genesis.json
 ADDRBOOK=https://ss-t.nibiru.nodestake.top/addrbook.json
-PORT=101
+PORT=39
+
 
 echo "export SOURCE=${SOURCE}" >> $HOME/.bash_profile
 echo "export WALLET=${WALLET}" >> $HOME/.bash_profile
 echo "export BINARY=${BINARY}" >> $HOME/.bash_profile
-echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
 echo "export CHAIN=${CHAIN}" >> $HOME/.bash_profile
 echo "export FOLDER=${FOLDER}" >> $HOME/.bash_profile
 echo "export VERSION=${VERSION}" >> $HOME/.bash_profile
-echo "export COSMOVISOR=${COSMOVISOR}" >> $HOME/.bash_profile
 echo "export REPO=${REPO}" >> $HOME/.bash_profile
+echo "export DENOM=${DENOM}" >> $HOME/.bash_profile
+echo "export COSMOVISOR=${COSMOVISOR}" >> $HOME/.bash_profile
 echo "export GENESIS=${GENESIS}" >> $HOME/.bash_profile
 echo "export ADDRBOOK=${ADDRBOOK}" >> $HOME/.bash_profile
 echo "export PORT=${PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
+
 
 # set vars input
 if [ ! $NODENAME ]; then
@@ -71,6 +77,20 @@ echo -e "Your Custom port: \e[1m\e[32m$PORT\e[0m"
 echo '================================================='
 sleep 2
 
+# Set Vars
+if [ ! $NODENAME ]; then
+	read -p "hello@nodexcapital:~# [ENTER YOUR NODE] > " NODENAME
+	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
+fi
+echo ""
+echo -e "YOUR NODE NAME : \e[1m\e[35m$NODENAME\e[0m"
+echo -e "NODE CHAIN CHAIN  : \e[1m\e[35m$CHAIN\e[0m"
+echo -e "NODE PORT      : \e[1m\e[35m$PORT\e[0m"
+echo ""
+
+    # Update
+sudo apt update && sudo apt upgrade -y
+
     # Installing dependencies
 	echo -e "\e[1m\e[32m1. Updating packages... \e[0m" && sleep 1
 sudo apt -q update
@@ -84,6 +104,7 @@ curl -Ls https://go.dev/dl/go1.19.5.linux-amd64.tar.gz | sudo tar -xzf - -C /usr
 eval $(echo 'export PATH=$PATH:/usr/local/go/bin' | sudo tee /etc/profile.d/golang.sh)
 eval $(echo 'export PATH=$PATH:$HOME/go/bin' | tee -a $HOME/.profile)
 
+
     # download and build binaries
 	echo -e "\e[1m\e[32m3. Downloading and building binaries... \e[0m" && sleep 1
 cd $HOME
@@ -94,22 +115,23 @@ git checkout $VERSION
 make build
 go install cosmossdk.io/tools/cosmovisor/cmd/cosmovisor@v1.4.0
 
+
 	# install & build cosmovisor
 	echo -e "\e[1m\e[32m4. Install & build cosmovisor... \e[0m"
-# mkdir -p $HOME/$FOLDER/$COSMOVISOR/genesis/bin
-# mv build/$BINARY $HOME/$FOLDER/$COSMOVISOR/genesis/bin/
-# rm -rf build
+mkdir -p $HOME/$FOLDER/$COSMOVISOR/genesis/bin
+mv build/$BINARY $HOME/$FOLDER/$COSMOVISOR/genesis/bin/
+rm -rf build
 
     # Create application symlinks
 	echo -e "\e[1m\e[32m5. Create App symlinks... \e[0m"
-# ln -s $HOME/$FOLDER/$COSMOVISOR/genesis $HOME/$FOLDER/$COSMOVISOR/current
-# ln -s $HOME/$FOLDER/$COSMOVISOR/current/bin/$BINARY /usr/bin/$BINARY
+ln -s $HOME/$FOLDER/$COSMOVISOR/genesis $HOME/$FOLDER/$COSMOVISOR/current
+sudo ln -s $HOME/$FOLDER/$COSMOVISOR/current/bin/$BINARY /usr/bin/$BINARY
 
-    # Init generation
+    # Init gen
     echo -e "\e[1m\e[32m6. Init config & Chain... \e[0m"
 $BINARY config chain-id $CHAIN
 $BINARY config keyring-backend test
-$BINARY config node tcp://localhost:${PORT}657
+$BINARY config node tcp://localhost:${PORT}57
 $BINARY init $NODENAME --chain-id $CHAIN
 
     # Set peers and seeds
@@ -121,18 +143,15 @@ sed -i -e "s|^seeds *=.*|seeds = \"$SEEDS\"|" $HOME/$FOLDER/config/config.toml
 
     # Download genesis and addrbook
     echo -e "\e[1m\e[32m8. Download genesis & addrbook... \e[0m" && sleep 1
-rm -rf $HOME/$FOLDER/config/genesis.json
-rm -rf $HOME/$FOLDER/config/addrbook.json
 curl -Ls $GENESIS > $HOME/$FOLDER/config/genesis.json
 curl -Ls $ADDRBOOK > $HOME/$FOLDER/config/addrbook.json
 
-
-	echo -e "\e[1m\e[32m7. Set custom ports, pruning & snapshots configuration ...\e[0m" && sleep 1
     # Set custom ports
+	echo -e "\e[1m\e[32m9. Set ports, pruning & snapshots configuration ...\e[0m" && sleep 1
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/$FOLDER/config/config.toml
 sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}091\"%" $HOME/$FOLDER/config/app.toml
 
-    # Set Config Pruning
+    # Set app.tom configuration
 pruning="custom"
 pruning_keep_recent="100"
 pruning_keep_every="0"
@@ -142,30 +161,27 @@ sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_rec
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/$FOLDER/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/$FOLDER/config/app.toml
 
-    # Set minimum gas price
+
 sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0.0001$DENOM\"/" $HOME/$FOLDER/config/app.toml
 
-
-    # Enable snapshot
-    sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"2000\"/" $HOME/$FOLDER/config/app.toml
+    # Enable snapshots
+sed -i -e "s/^snapshot-interval *=.*/snapshot-interval = \"2000\"/" $HOME/$FOLDER/config/app.toml
 $BINARY tendermint unsafe-reset-all --home $HOME/$FOLDER --keep-addr-book
 curl -L https://nibiru-t.service.indonode.net/nibiru-snapshot.tar.lz4 | tar -Ilz4 -xf - -C $HOME/$FOLDER
 
-
-
     # Create Service
     echo -e "\e[1m\e[32m8. Creating service file...\e[0m" && sleep 1
-sudo tee /etc/systemd/system/$BINARY.service > /dev/null <<EOF
+sudo tee /etc/systemd/system/$BINARY.service > /dev/null << EOF
 [Unit]
 Description=$BINARY
 After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=$(which $BINARY) start --home $HOME/FOLDER
+ExecStart=$(which cosmovisor) run start
 Restart=on-failure
-RestartSec=5
-LimitNOFILE=4096
+RestartSec=10
+LimitNOFILE=65535
 Environment="DAEMON_HOME=$HOME/$FOLDER"
 Environment="DAEMON_NAME=$BINARY"
 Environment="UNSAFE_SKIP_BACKUP=true"
@@ -174,12 +190,11 @@ Environment="UNSAFE_SKIP_BACKUP=true"
 WantedBy=multi-user.target
 EOF
 
-    # Register And Start Service
-sudo systemctl start $BINARY
-sudo systemctl daemon-reload
-sudo systemctl enable $BINARY
+# Register And Start Service
+systemctl daemon-reload
+systemctl enable $BINARY
+systemctl restart $BINARY
 
-    # Enable snapshots
 
 echo -e "\e[1m\e[35m================ KELAR CUY, JAN LUPA BUAT WALLET & REQ FAUCET ====================\e[0m"
 echo ""
