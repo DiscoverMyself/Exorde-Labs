4f9b836a1daafcce1aa7a8f7e15811ef631504d7
Aprm <78480857+DiscoverMyself@users.noreply.github.com>
2024-01-11 10:58:59 +0700

    Update artela.sh

--- src/artela.sh
+++ src/artela.sh
@@ -1,26 +1,19 @@
 #!/usr/bin/env bash
 exists()
 {
-  command -v "$1" >/dev/null 2>&1
+  command -v "$1" > /dev/null 2>&1
 }
-if exists curl; then
-echo ''
-else
-  sudo apt update && sudo apt install curl -y < "/dev/null"
-fi
-
 if exists go; then
 echo ''
 else
-  cd $HOME
-  curl https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf - 
+  sudo curl https://dl.google.com/go/go1.20.5.linux-amd64.tar.gz | sudo tar -C/usr/local -zxvf - > /dev/null
   cat <<'EOF' >>$HOME/.profile
-  export GOROOT=/usr/local/go
-  export GOPATH=$HOME/go
-  export GO111MODULE=on
-  export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
-  EOF
-  source $HOME/.profile -y < "/dev/null"
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export GO111MODULE=on
+export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin
+EOF
+source $HOME/.profile > /dev/null
 fi
 
 clear
@@ -36,12 +29,13 @@ sleep 0.5
 
 # Download Binaries, Genesis & Loader
 cd $HOME > /dev/null 2>&1
-git clone https://github.com/DiscoverMyself/sh-spinner 2>&1
-mv $HOME/sh-spinner/spinner.sh $HOME/ > /dev/null
-chmod +x sh-pinner.sh > /dev/null
+git clone https://github.com/DiscoverMyself/sh-spinner > /dev/null 2>&1
+mv $HOME/sh-spinner/spinner.sh $HOME/ > /dev/null 2>&1
+chmod +x spinner.sh > /dev/null 2>&1
+rm -rf sh-spinner > /dev/null 2>&1
 rm -rf artela > /dev/null 2>&1
 git clone https://github.com/artela-network/artela > /dev/null 2>&1
-cd artela > /dev/null 2>&1
+cd $HOME/artela
 git checkout v0.4.7-rc4 > /dev/null 2>&1
 make install > /dev/null 2>&1
 curl -Ls https://ss-t.artela.nodestake.org/genesis.json > $HOME/.artelad/config/genesis.json > /dev/null 2>&1
@@ -61,7 +55,8 @@ artelad init $nodename --chain-id artela_11822-1 > /dev/null 2>&1
 ./spinner.sh "sleep 5" "..." "Initialize your node"
 
 # Create Config File
-sudo tee /etc/systemd/system/artelad.service > /dev/null 2>&1 <<EOF
+sudo tee /etc/systemd/system/artelad.service > /dev/null
+EOF
 [Unit]
 Description=artelad Daemon
 After=network-online.target
@@ -81,9 +76,9 @@ artelad tendermint unsafe-reset-all --home ~/.artelad/ --keep-addr-book > /dev/n
 
 SNAP_RPC="https://rpc-t.artela.nodestake.org:443" > /dev/null 2>&1
 LATEST_HEIGHT=$(curl -s $SNAP_RPC/block | jq -r .result.block.header.height); \ > /dev/null 2>&1
-BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \ /dev/null 2>&1
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \ > /dev/null 2>&1
 TRUST_HASH=$(curl -s "$SNAP_RPC/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash) > /dev/null 2>&1
-echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH /dev/null 2>&1
+echo $LATEST_HEIGHT $BLOCK_HEIGHT $TRUST_HASH > /dev/null 2>&1
 sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \ > /dev/null 2>&1
 s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC,$SNAP_RPC\"| ; \ > /dev/null 2>&1
 s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \ > /dev/null 2>&1
@@ -94,21 +89,21 @@ more ~/.artelad/config/config.toml | grep 'trust_hash' > /dev/null 2>&1
 ./spinner.sh "sleep 7" "..." "Syncing your node to the latest block"
 
 # Set Custom Port
-FOLDER=.artelad /dev/null 2>&1
+FOLDER=.artelad > /dev/null 2>&1
 sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${PORT}660\"%" $HOME/$FOLDER/config/config.toml > /dev/null 2>&1
 sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${PORT}317\"%; s%^address = \":8080\"%address = \":${PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${PORT}091\"%" $HOME/$FOLDER/config/app.toml > /dev/null 2>&1
 
 
 
 # Enable Systemctl for Artela services
-sudo systemctl daemon-reload /dev/null 2>&1
-sudo systemctl enable artelad /dev/null 2>&1
-sudo systemctl restart artelad /dev/null 2>&1
+sudo systemctl daemon-reload > /dev/null 2>&1
+sudo systemctl enable artelad > /dev/null 2>&1
+sudo systemctl restart artelad > /dev/null 2>&1
 ./spinner.sh "sleep 3" "..." "Enable Systemctl for Artela services"
 
 
 # Start The Node
-./start-rbn.sh > /dev/null
+sudo systemctl restart artelad > /dev/null 2>&1
 ./spinner.sh "sleep 10" "..." "Running the Node"
 
 # Result
