5d4c4d02b04e6073872d3eb1355710baeb9eca4a
Aprm <78480857+DiscoverMyself@users.noreply.github.com>
2024-01-11 11:42:19 +0700

    Update artela.sh

--- src/artela.sh
+++ src/artela.sh
@@ -41,19 +41,19 @@ rm -rf sh-spinner > /dev/null 2>&1
 rm -rf artela > /dev/null 2>&1
 git clone https://github.com/artela-network/artela > /dev/null 2>&1
 cd $HOME/artela && git checkout v0.4.7-rc4 && make install > /dev/null 2>&1
-./spinner.sh "sleep 5" "..." "Download Binaries, Genesis & Addrbook"
+$HOME/spinner.sh "sleep 5" "..." "Download Binaries, Genesis & Addrbook"
 
 #Initialize node
 artelad config chain-id artela_11822-1 > /dev/null
 artelad config keyring-backend test > /dev/null
 artelad config node tcp://localhost:${PORT}657 > /dev/null
 artelad init $nodename --chain-id artela_11822-1 > /dev/null
-./spinner.sh "sleep 5" "..." "Initialize your node"
+$HOME/spinner.sh "sleep 5" "..." "Initialize your node"
 
 # Download Genesis & Addrbook file
 curl -Ls https://ss-t.artela.nodestake.org/genesis.json > $HOME/.artelad/config/genesis.json > /dev/null
 curl -Ls https://ss-t.artela.nodestake.org/addrbook.json > $HOME/.artelad/config/addrbook.json > /dev/null
-./spinner.sh "sleep 3" "..." "Download Genesis & Addrbook file"
+$HOME/spinner.sh "sleep 3" "..." "Download Genesis & Addrbook file"
 
 # Create Config File
 sudo tee /etc/systemd/system/artelad.service > /dev/null
@@ -70,7 +70,7 @@ LimitNOFILE=65535
 [Install]
 WantedBy=multi-user.target
 EOF
-./spinner.sh "sleep 3" "..." "Create Config File"
+$HOME/spinner.sh "sleep 3" "..." "Create Config File"
 
 # State Sync
 artelad tendermint unsafe-reset-all --home ~/.artelad/ --keep-addr-book > /dev/null 2>&1
@@ -87,7 +87,7 @@ s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" ~/.artelad/confi
 more ~/.artelad/config/config.toml | grep 'rpc_servers' > /dev/null 2>&1
 more ~/.artelad/config/config.toml | grep 'trust_height' > /dev/null 2>&1
 more ~/.artelad/config/config.toml | grep 'trust_hash' > /dev/null 2>&1
-./spinner.sh "sleep 7" "..." "Syncing your node to the latest block"
+$HOME/spinner.sh "sleep 7" "..." "Syncing your node to the latest block"
 
 # Set Custom Port
 FOLDER=.artelad > /dev/null 2>&1
@@ -100,16 +100,17 @@ sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${P
 sudo systemctl daemon-reload > /dev/null 2>&1
 sudo systemctl enable artelad > /dev/null 2>&1
 sudo systemctl restart artelad > /dev/null 2>&1
-./spinner.sh "sleep 3" "..." "Enable Systemctl for Artela services"
+$HOME/spinner.sh "sleep 3" "..." "Enable Systemctl for Artela services"
 
 
 # Start The Node
 sudo systemctl restart artelad > /dev/null 2>&1
-./spinner.sh "sleep 10" "..." "Running the Node"
+$HOME/spinner.sh "sleep 10" "..." "Running the Node"
 
 # Result
 echo '=============================== ALL SET !!! ==============================='
-echo -e "\e[1;32m Check your node status: \e[0m\e[1;36m${CYAN} pgrep rbbc ${NC}\e[0m"
-echo -e "\e[1;32m Check your nodelogs  : \e[0m\e[1;36m${CYAN} cat ./logs/rbbcLogs ${NC}\e[0m"
+echo -e "\e[1;32m Check your node status: \e[0m\e[1;36m${CYAN} systemctl status artelad ${NC}\e[0m"
+echo -e "\e[1;32m Check your nodelogs  : \e[0m\e[1;36m${CYAN} journalctl -fu artelad -o ${NC}\e[0m"
+echo -e "\e[1;32m Check your nodelogs  : \e[0m\e[1;36m${CYAN} artelad status 2>&1 | jq .SyncInfo ${NC}\e[0m"
 echo '======================== THANK FOR YOUR SUPPORT ==========================='
-echo -e '===============' "\e[0m\e[1;35m Support Us! => contact@quadblock.net \e[0m" '=================='
+echo -e '===============' "\e[0m\e[1;35m Join our TG: t.me/HappyCuanAirdrop \e[0m" '=================='
