969efde5c438bb03150860d66325a6bdde729a7c
Aprm <78480857+DiscoverMyself@users.noreply.github.com>
2024-01-11 11:07:45 +0700

    Update artela.sh

--- src/artela.sh
+++ src/artela.sh
@@ -28,6 +28,7 @@ read -r -p "Enter your custom Port (Default: 26): " PORT
 sleep 0.5
 
 # Update system and install build tools
+echo 'Installing prerequisites. take your cofee while waiting...'
 sudo apt update > /dev/null 2>&1
 sudo apt-get install git curl build-essential make jq gcc snapd chrony lz4 tmux unzip bc -y > /dev/null 2>&1
 
